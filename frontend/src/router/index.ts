7efb0397ea70213208808dfaa9812d783ede707a
tanximsiam <tanzimsjs@gmail.com>
2025-08-16 16:47:43 +0600

    Polished Frontend Redirect

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { useAuth } from '../stores/auth.ts'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
+<<<<<<< HEAD
 
 import UserDashboard from '../views/UserDashboard.vue'
 import OathHandler from '../views/OathHandler.vue'
@@ -11,6 +12,13 @@ import NgoProfileUpdate from '@/views/NgoProfileUpdate.vue'
 import DonationReportsView from '@/views/DonationReportsView.vue'
 
 
+=======
+<<<<<<< HEAD
+import UserDashboard from '../views/UserDashboard.vue'
+=======
+import OathHandler from '../views/OathHandler.vue'
+>>>>>>> 7428c91 (Polished Frontend Redirect)
+>>>>>>> f79510d (Polished Frontend Redirect)
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -59,6 +67,24 @@ const router = createRouter({
       path: '/aid-support',
       name: 'AidSupport',
       component: () => import('../views/AidSupport.vue'),
+<<<<<<< HEAD
+=======
+=======
+        {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: UserDashboard,
+<<<<<<< HEAD
+>>>>>>> 19af55d (Added a simple userdashboard and made AidReqform)
+=======
+=======
+    {
+      path: '/oauth/callback',
+      name: 'OauthCallback',
+      component: OathHandler,
+>>>>>>> 7428c91 (Polished Frontend Redirect)
+>>>>>>> 9a33270 (Polished Frontend Redirect)
+>>>>>>> f79510d (Polished Frontend Redirect)
     },
     {
       path: '/tasks',
