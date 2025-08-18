5050982f26dd893334523e8d764e5850c7995bea
tanximsiam <tanzimsjs@gmail.com>
2025-08-18 12:28:46 +0600

    Added aidreq_tracker

---
+++ frontend/src/views/MyRequestsView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import MyRequestsList from '@/components/MyRequestsList.vue'
+</script>
+
+<template>
+  <div class="max-w-3xl mx-auto p-4 md:p-6">
+    <h1 class="text-xl font-semibold mb-4">My Aid Requests</h1>
+    <MyRequestsList />
+  </div>
+</template>
