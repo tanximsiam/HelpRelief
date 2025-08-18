2f3d1b80a3804a09614965102948c45c89c8002e
tanximsiam <tanzimsjs@gmail.com>
2025-08-18 12:35:32 +0600

    bugfix

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
+import { computed } from 'vue'
 import AppLink from '@/components/AppLink.vue';
 import PrimaryButton from '@/components/PrimaryButton.vue'
+import { useAuth } from '@/stores/auth'
+const auth = useAuth()
+
+const offerHelpLink = computed(() =>
+  auth.isAuthenticated ? '/aid-support' : '/login'
+)
 
 </script>
 
@@ -11,7 +18,7 @@ import PrimaryButton from '@/components/PrimaryButton.vue'
         <h1 class="text-5xl font-bold text-blue-600">Connecting Aid with Urgency.</h1>
         <p class="text-2xl/10 font-medium text-slate-900">In times of crisis, every second counts. HelpRelief bridges NGOs, volunteers, and those in need— ensuring faster, smarter, and more coordinated disaster response across Bangladesh.</p>
         <div class="flex gap-6">
-          <PrimaryButton variant="primary" to="/donate">Offer Help</PrimaryButton>
+          <PrimaryButton variant="primary" :to="offerHelpLink">Offer Help</PrimaryButton>
           <AppLink variant="primary" to="/about">or report a disaster</AppLink>
         </div>
       </div>
