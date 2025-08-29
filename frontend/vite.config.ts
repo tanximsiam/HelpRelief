657e0efae97b00d937dbbbe8c2a08883a6c4df1b
tanximsiam <tanzimsjs@gmail.com>
2025-08-30 04:34:12 +0600

    Fixes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,6 +8,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' }
+  },
   plugins: [
     vue(),
     vueDevTools(),
