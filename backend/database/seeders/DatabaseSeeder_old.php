18a94e877377923ca77fcaad1d4681169a9b3b45
ArifXO <khaniftekhar846@gmail.com>
2025-08-29 19:42:56 +0600

    fix tasklogs and remove scroll

--- backend/database/seeders/DatabaseSeeder_old.php
+++ backend/database/seeders/DatabaseSeeder_old.php
@@ -30,7 +30,6 @@ class DatabaseSeeder extends Seeder
             TaskSeeder::class,
             AidSupportSeeder::class,
             VolunteerSeeder::class,
-            VolunteerTaskLogsSeeder::class,
             DonationReportSeeder::class,
         ]);
 
